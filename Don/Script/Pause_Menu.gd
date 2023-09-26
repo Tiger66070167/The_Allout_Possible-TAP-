44a1591b4278e1d414e03fc346a9a37f0532bd23
DobDoiDongDang <66070225@kmitl.ac.th>
2023-09-26 12:42:36 +0700

    Update parallax and pause menu in parallax scene

--- Don/Script/Pause_Menu.gd
+++ Don/Script/Pause_Menu.gd
@@ -1,13 +1,14 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().paused = false
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ESC"):
-		get_tree().paused = not get_tree().paused
-		$Pause_Menu.visible = not $Pause_Menu.visible
+	pass
+
+func _on_button_pressed():
+	get_tree().quit()
