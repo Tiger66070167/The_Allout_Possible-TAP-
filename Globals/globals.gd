7410754c489f9f646711e6034498b06f0d48240b
DobDoiDongDang <66070225@kmitl.ac.th>
2023-10-01 11:37:48 +0700

    Change version of Godot to 3.5.2
    Cause 4.11 cannot use PythonScript plugin. Then we need to change version to 3.5.2 and recreate everything in 3.5.2

--- Globals/globals.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var health : int = 4
-var screen_move_x : int = 0
--- Tiger/Script/ui.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-@onready var health_ui: TextureProgressBar = $Health/TextureProgressBar
-
-func health():
-	health_ui.value = Globals.health
-
