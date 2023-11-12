60a38cfcb1b09d2cae27aef5fc0f66e53413866e
Tiger66070167 <139055235+Tiger66070167@users.noreply.github.com>
2023-11-12 14:09:41 +0700

    audio setting
    audio is too loud

--- Overlay_menu/Script/Control.gd
+++ Overlay_menu/Script/Control.gd
@@ -11,3 +11,14 @@ func _on_CheckButton_toggled(button_pressed):
 
 func _on_exit_pressed():
 	$"..".visible = false
+
+
+func _on_HSlider_value_changed(value):
+	set_bus_volume(0, value)
+
+func _on_HSlider2_value_changed(value):
+	set_bus_volume(1, value)
+
+func set_bus_volume(bus_index: int, value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear2db(value))
+	AudioServer.set_bus_mute(bus_index, value < 0.01)
